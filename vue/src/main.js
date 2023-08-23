6e7a45f99e789a988c81765bc35eec297e32e398
Jeffrey Bee <jeffreypbee@gmail.com>
2023-08-23 14:30:13 -0400

    Added Font Awesome icons

--- vue/src/main.js
+++ vue/src/main.js
@@ -4,6 +4,55 @@ import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faGhost } from '@fortawesome/free-solid-svg-icons'
+import { faGear } from '@fortawesome/free-solid-svg-icons'
+import { faDroplet } from '@fortawesome/free-solid-svg-icons'
+import { faFire } from '@fortawesome/free-solid-svg-icons'
+import { faSnowflake } from '@fortawesome/free-solid-svg-icons'
+import { faFeather } from '@fortawesome/free-solid-svg-icons'
+import { faBug } from '@fortawesome/free-solid-svg-icons'
+import { faMask } from '@fortawesome/free-solid-svg-icons'
+import { faSeedling } from '@fortawesome/free-solid-svg-icons'
+import { faDragon } from '@fortawesome/free-solid-svg-icons'
+import { faWandSparkles } from '@fortawesome/free-solid-svg-icons'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
+import { faSplotch } from '@fortawesome/free-solid-svg-icons'
+import { faSkull } from '@fortawesome/free-solid-svg-icons'
+import { faMound } from '@fortawesome/free-solid-svg-icons'
+import { faGem } from '@fortawesome/free-solid-svg-icons'
+import { faHandFist } from '@fortawesome/free-solid-svg-icons'
+import { faBoltLightning } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faGhost)
+library.add(faGear)
+library.add(faDroplet)
+library.add(faFire)
+library.add(faSnowflake)
+library.add(faFeather)
+library.add(faBug)
+library.add(faMask)
+library.add(faSeedling)
+library.add(faDragon)
+library.add(faWandSparkles)
+library.add(faEye)
+library.add(faSplotch)
+library.add(faSkull)
+library.add(faMound)
+library.add(faGem)
+library.add(faHandFist)
+library.add(faBoltLightning)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,6 +2,24 @@
   <div class="home">
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
+    <font-awesome-icon icon="fa-solid fa-ghost" />
+    <font-awesome-icon icon="fa-solid fa-gear" />
+    <font-awesome-icon icon="fa-solid fa-droplet" />
+    <font-awesome-icon icon="fa-solid fa-fire" />
+    <font-awesome-icon icon="fa-solid fa-snowflake" />
+    <font-awesome-icon icon="fa-solid fa-feather" />
+    <font-awesome-icon icon="fa-solid fa-bug" />
+    <font-awesome-icon icon="fa-solid fa-mask" />
+    <font-awesome-icon icon="fa-solid fa-seedling" />
+    <font-awesome-icon icon="fa-solid fa-dragon" />
+    <font-awesome-icon icon="fa-solid fa-wand-sparkles" />
+    <font-awesome-icon icon="fa-solid fa-eye" />
+    <font-awesome-icon icon="fa-solid fa-splotch" />
+    <font-awesome-icon icon="fa-solid fa-skull" />
+    <font-awesome-icon icon="fa-solid fa-mound" />
+    <font-awesome-icon icon="fa-solid fa-gem" />
+    <font-awesome-icon icon="fa-solid fa-hand-fist" />
+    <font-awesome-icon icon="fa-solid fa-bolt-lightning" />
   </div>
 </template>
 
