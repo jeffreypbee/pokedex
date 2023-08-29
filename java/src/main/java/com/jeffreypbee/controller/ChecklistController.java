d47f4c19b53c615f70fd0a2fe01bc708bbf527f8
Jeffrey Bee <jeffreypbee@gmail.com>
2023-08-29 19:46:14 -0400

    Added checklist controller

---
+++ java/src/main/java/com/jeffreypbee/controller/ChecklistController.java
@@ -0,0 +1,23 @@
+package com.jeffreypbee.controller;
+
+import com.jeffreypbee.dao.ChecklistDao;
+import com.jeffreypbee.model.Checklist;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class ChecklistController {
+
+    private ChecklistDao checklistDao;
+
+    public ChecklistController(ChecklistDao checklistDao) { this.checklistDao = checklistDao; }
+
+    @GetMapping(path="/users/{id}/checklists")
+    public List<Checklist> getAllForUser(@PathVariable int id) {
+        return checklistDao.getAllChecklistsForUser(id);
+    }
+
+
+}
